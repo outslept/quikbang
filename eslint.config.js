a44ebc586732a47147847a888685293452abaea3
outslept <webeugene@proton.me>
2025-03-06 16:28:02 +0300

    disabling no console

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,8 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   svelte: true,
+}, {
+  rules: {
+    'no-console': 'off',
+  },
 })
--- server.ts
+++ server.ts
@@ -1,25 +1,26 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'node:path'
+import process from 'node:process'
+import { fileURLToPath } from 'node:url'
+import express from 'express'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.use('/data', express.static(path.join(__dirname, 'data')));
+app.use('/data', express.static(path.join(__dirname, 'data')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(port, () => {
-    console.log(`Server runs on port ${port}`);
-  });
+    console.log(`Server runs on port ${port}`)
+  })
 }
 
-export default app;
+export default app
