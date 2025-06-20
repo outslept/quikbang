7d129af4b8e3e5602dc9220a7730c54d8aa301fb
outslept <webeugene@proton.me>
2025-06-20 09:49:57 +0300

    chore: inlining stuff

--- server.ts
+++ server.ts
@@ -1,25 +1,22 @@
-import path from 'node:path'
+import { dirname, join } from 'node:path'
 import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 import express from 'express'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
-const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'dist')))
-
-app.use('/data', express.static(path.join(__dirname, 'data')))
+app.use(express.static(join(__dirname, 'dist')))
+app.use('/data', express.static(join(__dirname, 'data')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(join(__dirname, 'dist', 'index.html'))
 })
 
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(port, () => {
-    console.log(`Server runs on port ${port}`)
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server runs on port ${process.env.PORT || 3000}`)
   })
 }
 
